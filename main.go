075f9c88df250f8e380e07fd02280c2ce4f96b06
soyaibzihad10 <mithila.anam123@gmail.com>
2025-05-10 15:02:16 +0600

    difference between Handle() and HandleFunc()

--- main.go
+++ main.go
@@ -5,19 +5,27 @@ import (
     "net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("This is the home page."))
+type homeHandler struct {
+}
+
+func (hh homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Welcome!"))
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("This is the about page."))
-}
+} 
 
 func main() {
     mux := http.NewServeMux()
 
-    mux.HandleFunc("/", homeHandler)
+    hh := homeHandler{}
+
+	// hh does implement ServeHTTP, just pass it to mux
+    mux.Handle("/home", hh)
+
+	// because aboutHandler does not implement ServeHTTP so we pass it to HandlerFunc cause HandlerFunc does implement ServeHTTP. So aboutHandler is now a handlerFunc
     mux.HandleFunc("/about", aboutHandler)
 
     log.Fatal(http.ListenAndServe(":8080", mux))
-}
+}    
