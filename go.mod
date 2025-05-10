449efb91b68853bbc49a004275a2271543b87011
soyaibzihad10 <mithila.anam123@gmail.com>
2025-05-10 15:27:33 +0600

    gorila-mux routing basics

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/soyaibzihad10/go-backend-app
 
 go 1.24.2
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type homeHandler struct {
 }
 
 func (hh homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Welcome!"))
+	w.Write([]byte("Welcome!"))
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("This is the about page."))
-} 
+	w.Write([]byte("This is the about page."))
+}
 
 func main() {
-    mux := http.NewServeMux()
+	r := mux.NewRouter()
 
-    hh := homeHandler{}
+	r.HandleFunc("/", homeHandler)
 
-	// hh does implement ServeHTTP, just pass it to mux
-    mux.Handle("/home", hh)
+	/*
+		/books/all এবং /books/{isbn} এই দুইটি route একে অপরের সঙ্গে conflict করতে পারে path matching এর সময়।
+		এখন, ইউজার যদি ব্রাউজারে /books/all হিট করে...
 
-	// because aboutHandler does not implement ServeHTTP so we pass it to HandlerFunc cause HandlerFunc does implement ServeHTTP. So aboutHandler is now a handlerFunc
-    mux.HandleFunc("/about", aboutHandler)
+		👉 রাউটার কীভাবে জানবে যে এটা all নামে একটি ফিক্সড রাউট, নাকি এটি {isbn} নামে কোনো স্ট্রিং ভ্যালু?
+		
+		Soulution: Go-এর gorilla/mux রাউটার উপরের নিয়মে কাজ করে:
+		It matches in order from top to bottom.
+	*/
 
-    log.Fatal(http.ListenAndServe(":8080", mux))
-}    
+	booksSSubR := r.PathPrefix("/books").Subrouter()
+	booksSubR.HandleFunc("/all", AllHandler).Methods(http.MethodGet)
+    booksSubR.HandleFunc("/{isbn}", IspnHandler).Methods(http.MethodGet)
+    booksSubR.HandleFunc("/new", NewHandler).Methods(http.MethodPost)
+    booksSubR.HandleFunc("/update", UpdateHandler).Methods(http.MethodPut)
+    booksSubR.HandleFunc("/delete/{isbn}", DeleteIspnHandler).Methods(http.MethodDelete)
+
+    log.Fatal(http.ListenAndServe(":8090", r))
+}
